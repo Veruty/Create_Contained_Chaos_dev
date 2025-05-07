38183b7d83381507afd2493fef8a1b40d3d5cb2e
Veruty <mihabeuan@gmail.com>
2025-05-07 22:07:21 +0700

    first working arc_furnace multiblock prototype

---
+++ kubejs/server_scripts/tags/can_replace_machine_casing.js
@@ -0,0 +1,5 @@
+ServerEvents.tags('block', event => {
+	event.add('kubejs:can_replace_machine_casing', 'mbd2:machine_casing_input')
+	event.add('kubejs:can_replace_machine_casing', 'mbd2:machine_casing_output')
+	event.add('kubejs:can_replace_machine_casing', 'mbd2:machine_casing_energy')
+})
