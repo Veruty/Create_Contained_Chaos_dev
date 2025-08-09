05f1b3f37a5a06c6e1cf85919cf3f942be005f23
Veruty <mihabeuan@gmail.com>
2025-08-09 07:45:54 +0700

    added more fuels to immersive genenerator

---
+++ kubejs/server_scripts/recipes/fixes/immersive_fuel_fix.js
@@ -0,0 +1,5 @@
+ServerEvents.recipes(e => {
+    e.custom({"type":"immersiveengineering:generator_fuel","burnTime":500,"fluidTag":"forge:diesel"})
+    e.custom({"type":"immersiveengineering:generator_fuel","burnTime":500,"fluidTag":"forge:gasoline"})
+    e.custom({"type":"immersiveengineering:generator_fuel","burnTime":60,"fluidTag":"forge:ethanol"})
+})
