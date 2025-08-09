5c2400652c332535129fcc2ac2d95c3377d5f01a
Veruty <mihabeuan@gmail.com>
2025-08-09 07:47:23 +0700

    byn steel pipe craft fix

--- kubejs/server_scripts/recipes/tweaks/blocksyouneed_tweaks.js
+++ kubejs/server_scripts/recipes/tweaks/blocksyouneed_tweaks.js
@@ -1,3 +1,16 @@
 ServerEvents.recipes(e => {
+	e.remove({id: 'blocksyouneed_luna:pipe_steel'})
+	e.remove({id: 'blocksyouneed_luna:rec_pipe_steel_c'})
 	e.recipes.create.mixing('2x blocksyouneed_luna:moonstone', ['minecraft:lapis_lazuli', '#forge:blocks/stone'])
+	e.shaped(
+    Item.of('blocksyouneed_luna:steel_pipe', 6),
+    [
+        '  A',
+        ' A ',
+        '   '
+    ],
+    {
+        A: '#forge:ingots/steel'
+    }
+)
 })
