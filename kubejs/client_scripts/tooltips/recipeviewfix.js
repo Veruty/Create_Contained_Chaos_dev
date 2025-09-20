8967da4e07e2d34562b3aaf95f2a89b49cb19f93
Veruty <mihabeuan@gmail.com>
2025-09-20 20:10:33 +0700

    jei tooltip for chromatic mats

---
+++ kubejs/client_scripts/tooltips/recipeviewfix.js
@@ -0,0 +1,5 @@
+JEIEvents.information(e => {
+  e.addItem("create:refined_radiance", ['Craftable by dropping Chromatic Compound in activated beacon beam.'])
+  e.addItem("create:shadow_steel", ['Craftable by dropping Chromatic Compound in the void.'])
+});
+// 20.09.25 Veruty: stopgap until i find how to show recipes normally (probably wont be fixed ever)
