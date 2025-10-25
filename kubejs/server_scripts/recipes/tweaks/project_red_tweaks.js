77ebe4060e5ff715a0eecfacd940406c26f452bb
Veruty <mihabeuan@gmail.com>
2025-10-26 06:06:07 +0700

    asset change

---
+++ kubejs/server_scripts/recipes/tweaks/project_red_tweaks.js
@@ -0,0 +1,4 @@
+ServerEvents.recipes(event => {
+  event.recipes.create.haunting('projectred_core:electrotine_dust', 'minecraft:redstone')
+
+})
--- kubejs/server_scripts/recipes/tweaks/thermal_tweaks.js
+++ kubejs/server_scripts/recipes/tweaks/thermal_tweaks.js
@@ -138,6 +138,7 @@ ServerEvents.recipes(e => {
     })
     e.remove({ id: 'thermal:machine_crafter' })
 	e.remove({ id: 'thermal:fire_charge/signalum_ingot_4' })
+	e.remove({ id: 'thermal:fire_charge/obsidian_glass_2' })
 	e.remove({ id: 'thermal:smelting/signalum_ingot_from_dust_smelting' })
 	e.remove({ id: 'thermal:smelting/signalum_ingot_from_dust_blasting' })
 	e.remove({ id: 'thermal:fire_charge/lumium_ingot_4' })
@@ -207,26 +208,29 @@ e.shaped(
         D: 'thermal:rf_coil'
     }
 )
+ e.recipes.create.mixing('2x thermal:obsidian_glass', ['#forge:obsidian', 'minecraft:quartz', 'minecraft:sand']).superheated()
+e.remove({ id: 'thermal:machine_frame' })
 e.custom({
-  "type": "thermal:pyrolyzer",
-  "ingredient": {
-    "item": "minecraft:charcoal"
-  },
-  "result": [
-    {
-      "item": "thermal:coal_coke"
-    },
-    {
-      "item": "thermal:tar",
-      "chance": 0.5
-    },
-    {
-      "fluid": "thermal:creosote",
-      "amount": 500
-    }
+  "type": "extendedcrafting:shaped_table",
+  "pattern": [
+    "ABA",
+    "BCB",
+    "ABA"
   ],
-  "energy": 4000,
-  "experience": 0.15
+  "key": {
+    "A": {
+      "item": "thermal:invar_plate"
+    },
+    "B": {
+      "item": "thermal:obsidian_glass"
+    },
+    "C": {
+      "item": "thermal:invar_gear"
+    }
+  },
+  "result": {
+    "item": "thermal:machine_frame"
+  }
 })
 
 })
