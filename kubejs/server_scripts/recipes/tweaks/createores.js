860499d7ef3008dcae0f3b0064b58180720f2fe6
Veruty <mihabeuan@gmail.com>
2025-10-29 21:40:14 +0700

    thank vs for not stating oil thing

--- kubejs/server_scripts/recipes/tweaks/createores.js
+++ kubejs/server_scripts/recipes/tweaks/createores.js
@@ -39,4 +39,36 @@ event.custom({
   "vein_id": "kubejs:raw_osmium_ore"
 })
 
+event.custom({
+  "type": "createoreexcavation:vein",
+  "amountMax": 40.0,
+  "amountMin": 10.0,
+  "biomeWhitelist": "createdieselgenerators:oil_biomes",
+  "icon": {
+    "item": 'createdieselgenerators:crude_oil_bucket'
+  },
+  "name": "Crude Oil",
+  //change later to translate key
+  "placement": {
+    "salt": 1485901891,
+    "separation": 8,
+    "spacing": 256
+  },
+  "priority": 0
+}).id("kubejs:crude_oil")
+
+event.custom({
+  "type": "createoreexcavation:extracting",
+  "drill": {
+    "tag": "createoreexcavation:drills"
+  },
+  "output": {
+    "amount": 1000,
+    "fluid": "createdieselgenerators:crude_oil"
+  },
+  "priority": 0,
+  "stress": 256,
+  "ticks": 20,
+  "vein_id": "kubejs:crude_oil"
+})
 })
