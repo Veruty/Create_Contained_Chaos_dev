3fef2e7829c6c1652d81eab37c016b86d9b7d50b
Veruty <no>
2026-02-01 11:56:59 +0700

    fix for create diesel not being to make pumpjack head

--- kubejs/server_scripts/recipes/tweaks/create_diesel.js
+++ kubejs/server_scripts/recipes/tweaks/create_diesel.js
@@ -1,17 +1,4 @@
 ServerEvents.recipes(event => {
-   event.remove({ output: 'createdieselgenerators:pumpjack_head' })
-    event.shaped(
-    Item.of('createdieselgenerators:pumpjack_head'),
-    [
-        'A A',
-        'BCB',
-        'A A'
-    ],
-    {
-        B: 'create:zinc_ingot',
-        A: 'create:andesite_alloy',
-        C: '#kubejs:create_kelp_rubber'
-    })
     event.remove({ id: 'createdieselgenerators:crafting/asphalt_block' })
     event.shaped(
     Item.of('createdieselgenerators:asphalt_block', 8),
